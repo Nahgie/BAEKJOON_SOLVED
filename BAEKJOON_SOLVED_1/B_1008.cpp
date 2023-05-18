f65c43097207c3e0fb00dc1054740cae5720eff7
Nahgie <133487853+Nahgie@users.noreply.github.com>
2023-05-18 17:01:04 +0900

    입출력과 사칙연산

---
+++ BAEKJOON_SOLVED_1/B_1008.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main(void) {
+
+	int a = 0, b = 0;
+
+	std::cin >> a >> b;
+
+	std::cout.precision(12); //자릿수 출력 문제 때문에 범위 지정 및 수정
+	std::cout << std::fixed << (double)a / (double)b << std::endl;
+
+	return 0;
+}
+
+
+
---
+++ BAEKJOON_SOLVED_1/B_18108.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+	int num = 0;
+
+	std::cin >> num;
+	std::cout << num - 543;
+
+	return 0;
+}
