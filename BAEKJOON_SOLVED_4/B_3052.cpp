b1990deca2928d258319ec1ac25b2c9300caadbb
Nahgie <133487853+Nahgie@users.noreply.github.com>
2023-06-21 15:25:20 +0900

    1차원 배열

---
+++ BAEKJOON_SOLVED_4/B_3052.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main() {
+    int num[10];
+    bool arr[42] = { 0, };
+    int cnt = 0;
+
+    for (int i = 0; i < 10; i++) {
+
+        std::cin >> num[i];
+        arr[num[i] % 42]++;
+
+    }
+
+    for (int i = 0; i < 42; i++) {
+        //std::cout << i << " : " << arr[i] << std::endl; //디버그용 코드
+        if (arr[i] > 0) {
+            cnt++;
+        }
+    }
+
+    std::cout << cnt << std::endl;
+
+    return 0;
+}
